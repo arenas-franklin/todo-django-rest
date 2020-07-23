5be00694bf4d68dd29560569337792dd868106d0
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 18:16:10 -0300

    criado url para o delete e put

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,6 +1,7 @@
-from app.views import todo_list
+from app.views import todo_list, todo_detail_change_and_delete
 from django.urls import path
 
 urlpatterns = [
     path('', todo_list),
+    path('<int:pk>/', todo_detail_change_and_delete),
 ]
