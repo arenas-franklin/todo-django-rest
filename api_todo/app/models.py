1df56ab3768fc97aa78875c7b0acc801b4c82908
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 16:35:57 -0300

    criado a class todo

--- api_todo/app/models.py
+++ api_todo/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    name = models.CharField(max_length=120)
+    done = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
