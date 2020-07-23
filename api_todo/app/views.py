99f8dae67d407b71b4939dcc5416d527c2c6ea02
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 17:32:50 -0300

    creado metodo get para listas todos dos dados

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from app.models import Todo
+from app.serializers import TodoSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def todo_list(request):
+    todo = Todo.objects.all()
+    serializer = TodoSerializer(todo, many=True)
+    return Response(serializer.data)
