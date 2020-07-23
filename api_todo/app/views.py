402a97f7c257ea0ed13525fbd55163f48759e99c
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 19:01:02 -0300

    refatorando os method HTTP com generics

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,6 +1,7 @@
 from app.models import Todo
 from app.serializers import TodoSerializer
 
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.exceptions import NotFound 
 from rest_framework.decorators import api_view
@@ -9,41 +10,11 @@ from rest_framework.views import APIView
 
 # Create your views here.
 
-class TodoListAndCreate(APIView):
-    def get(self, request):
-        todo = Todo.objects.all()   
-        serializer = TodoSerializer(todo, many=True)
-        return Response(serializer.data)
+class TodoListAndCreate(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
-    def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TodoDetailChangeDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
-class TodoDetailChangeDelete(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Todo.objects.get(pk=pk)
-        except Todo.DoesNotExist:
-            raise NotFound()
-
-    def get(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        todo = self.get_object(pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
