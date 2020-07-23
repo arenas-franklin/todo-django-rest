7a362ea9996f768d26032e7530c7f2bb614df9b8
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 16:47:48 -0300

    mudado created_at para registrar horario da tarefa

--- api_todo/app/models.py
+++ api_todo/app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=120)
     done = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
