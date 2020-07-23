c46192513e1f1324e043630a5a420551eae681ee
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-07-23 17:55:28 -0300

    criado metodo post para inserir um dado

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,13 +1,21 @@
 from app.models import Todo
 from app.serializers import TodoSerializer
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def todo_list(request):
-    todo = Todo.objects.all()
-    serializer = TodoSerializer(todo, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        todo = Todo.objects.all()   
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
